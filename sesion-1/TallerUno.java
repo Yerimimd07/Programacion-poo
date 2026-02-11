bd86c804fa835d1bc4b71d8dee75df7bb5522ce0
Yerimimd07 <yerimi.miranda@campusucc.edu.co>
2026-02-11 09:48:18 -0500

    Update TallerUno.java

--- sesion-1/TallerUno.java
+++ sesion-1/TallerUno.java
@@ -1,1 +1,16 @@
+public class SemanaDos {
+  public static void main(String args[]) {
 
+    //2,10 Calcular y visualizar la longitud de la circunferencia y el área de un circulo de radio dado 
+  double radio = 2.5;
+    /* Formulas 
+    *Longitud = 2⋅π⋅r
+    *Area = π⋅r2
+    */
+  double radio = 2
+  double longitud = 2 * Math.PI * radio;
+  double area = Math.PI * Math.pow(radio,2);
+    
+    
+  }
+}
