05a2b990c406240d1d0eb29c49f1f9d5c6c9a7c5
Yerimimd07 <yerimi.miranda@campusucc.edu.co>
2026-02-18 01:36:19 +0000

    punto 4.2

---
+++ sesion-1/Punto42.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Punto42 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+            double distancia, precioporKM = 2.5;
+            int diasEstadia;
+            // variables siempre en minisculas 
+
+
+
+
+
+        sc.close();
+    }
+}
