3de33212c4d6dea8365ea61953c9c78310c13c02
Yerimimd07 <yerimi.miranda@campusucc.edu.co>
2026-02-24 23:26:05 +0000

    Punto 42

--- POP/Sesion3/Punto42.java
+++ POP/Sesion3/Punto42.java
@@ -2,12 +2,29 @@ import java.util.Scanner;
 
 public class Punto42 {
     public static void main(String[] args) {
+
+        // variables siempre en minisculas
+        double distanciakm, preciototal;
+        int diasestadia;
+        final double precioKM = 2.5; // se usa final porque el pecio no cambia
+
         Scanner sc = new Scanner(System.in);
-            // variables siempre en minisculas 
 
+        System.out.println("Ingrese el número de días: ");
+        diasestadia = sc.nextInt();
 
+        System.out.println("Ingrese la distancia en Km: ");
+        distanciakm = sc.nextDouble();
 
+        // Cálculo del precio ida y vuelta
+        preciototal = diasestadia * 2 * distanciakm;
 
+        /*
+         * si distancia > 800 km. y duración > 7 días
+         * precio total = (distancia * 2.5) – 30/100 * (precio total).
+         */
+
+        
 
         sc.close();
     }
