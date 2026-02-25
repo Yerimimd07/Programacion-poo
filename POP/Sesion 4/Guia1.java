d1f1b01eda7318aef5278abae365f8b62e68a729
Yerimimd07 <yerimi.miranda@campusucc.edu.co>
2026-02-25 02:12:31 +0000

    punto 47 pag 161 terminado con for

--- POP/Sesion 4/Guia1.java
+++ POP/Sesion 4/Guia1.java
@@ -9,26 +9,34 @@ public class Guia1 {
          * de un colegio de acueerdo con la siguiente tabla:
          * Alumnos de menos de 40 kg
          * Alumnos entre 40 y 50 kg
-         * Alumnos de mas de 50 kg y menos de 60 kg
+         * Alumnos de ams de 50 kg y menos de 60 kg
          * Alumnos de mas o igual a 60 kg
          */
+        System.out.println("Cuantos alumnos desea ingresar: ");
+        int cantidadAlumnos = sc.nextInt();
+        for (int i = 1; i <= cantidadAlumnos; i++) {
 
-        System.out.println("Digite el peso: ");
-        int peso = sc.nextInt();
-        if (peso < 40) {
-            System.out.println("menos de 40 kg");
-        } else {
-            if (peso >= 40 && peso < 50) {
-                System.out.println("entre de 40 kg  y menos de 50 kg");
+            System.out.println("Digite el peso: ");
+            int peso = sc.nextInt();
+            if (peso < 40) {
+                System.out.println("menos de 40 kg");
             } else {
-                if (peso >= 50 && peso < 60) {
-                    System.out.println("mas de 50 kg y menos de 60 kg");
+                if (peso >= 40 && peso < 50) {
+                    System.out.println("entre de 40 kg  y menos de 50 kg");
                 } else {
-                    System.out.println("mas o igual a 60 kg");
+                    if (peso >= 50 && peso < 60) {
+                        System.out.println("mas de 50 kg y menos de 60 kg");
+                    } else {
+                        System.out.println("mas o igual a 60 kg");
+                    }
                 }
+                
             }
+            
+
         }
         sc.close();
+
     }
 
 }
