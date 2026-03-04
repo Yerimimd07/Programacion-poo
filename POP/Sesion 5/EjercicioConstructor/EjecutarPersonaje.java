d57a07b0d3812facffa3181edbf12dedca45002a
Yerimimd07 <yerimi.miranda@campusucc.edu.co>
2026-03-04 01:42:16 +0000

    Personaje completo

---
+++ POP/Sesion 5/EjercicioConstructor/EjecutarPersonaje.java
@@ -0,0 +1,11 @@
+public class EjecutarPersonaje {
+    public static void main(String[] args) {
+        
+        //creacion del objeto tipo personaje
+        Personaje p1 = new Personaje("Superman");
+        p1.mostrarInfo();
+        
+        
+    }
+    
+}
