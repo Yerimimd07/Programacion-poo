be591dc8507d4a3651b72a34a1ee90260c8d820a
Yerimimd07 <yerimi.miranda@campusucc.edu.co>
2026-03-05 17:05:04 +0000

    Personaje mas nivel

--- POP/Sesion 5/EjercicioConstructor/EjecutarPersonaje.java
+++ POP/Sesion 5/EjercicioConstructor/EjecutarPersonaje.java
@@ -2,7 +2,7 @@ public class EjecutarPersonaje {
     public static void main(String[] args) {
         
         //creacion del objeto tipo personaje
-        Personaje p1 = new Personaje("Superman");
+        Personaje p1 = new Personaje("Superman", 2);
         p1.mostrarInfo();
         
         
--- POP/Sesion 5/EjercicioConstructor/Personaje.java
+++ POP/Sesion 5/EjercicioConstructor/Personaje.java
@@ -6,13 +6,12 @@ public class Personaje {
     //METODO
     // Constructor que obliga a dar un nombre al crear el personaje
 
-    public Personaje ( String nombre ) {
-        this . nombre = nombre ;
-        this . nivel = 1; // Todos empiezan en nivel 1 por defecto
+    public Personaje ( String nombre, int nivel) {
+        this.nombre = nombre ;
+        this.nivel = nivel; // Todos empiezan en nivel 1 por defecto
         }
 
     public void mostrarInfo() {
-        System.out.println(" Heroe : " + nombre + " | Nivel : " +
-                nivel);
+        System.out.println(" Heroe : " + nombre + "Nivel : " + nivel);
     }
 }
