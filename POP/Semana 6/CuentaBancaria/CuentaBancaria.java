83b559a1770b463a217fa95c6f6d2329dd43d369
Yerimimd07 <yerimi.miranda@campusucc.edu.co>
2026-03-11 11:46:56 +0000

    banco en costructor

---
+++ POP/Semana 6/CuentaBancaria/CuentaBancaria.java
@@ -0,0 +1,17 @@
+public class CuentaBancaria{
+    //atributos
+    String numeroCuenta;
+    String titular;
+    double saldo;
+
+    //Constructor 
+
+    public CuentaBancaria(String numeroCuenta, String tirular, double saldo);
+    this.numeroCuenta = numeroCuenta;
+    this.titular = titular;
+    this.saldo = saldo;
+
+    public void 
+
+
+}
