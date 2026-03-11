fe8768934fa0d857d56d095072833233cfd38ea4
Yerimimd07 <yerimi.miranda@campusucc.edu.co>
2026-03-11 00:58:40 +0000

    inventario ok

---
+++ POP/Semana 6/EjecutarInventario.java
@@ -0,0 +1,19 @@
+public class EjecutarInventario {
+    public static void main(String[] args) {
+
+        Producto objp1 = new Producto("Teclado", 10012, 10, 230.0);
+        // Mostrar el contenido del objeto objp1
+        System.out.println(objp1);
+        //Agreagar  5 teclados y mostrar nuevamente el objeto
+        objp1.agregarStock(5);
+        System.out.println(objp1);
+
+        //Descontar 8 teclados y mostrar el objeto
+        objp1.reducirStock(8);
+        System.out.println(objp1);
+
+
+
+    }
+
+}
--- POP/Sesion 5/Usuario/EjecutarUsuario.java
+++ POP/Sesion 5/Usuario/EjecutarUsuario.java
@@ -3,18 +3,19 @@ public class EjecutarUsuario {
         // En el main, crea al usuario ”JavaCoderτ simula que gana 3 seguidores
         Usuario user1 = new Usuario("JavaCodert");
 
-        user1.nuevoSeguidor();
-        user1.nuevoSeguidor();
-        user1.nuevoSeguidor();
-        user1.nuevoSeguidor();
+       for(int i = 1; i <= 3; i++){
+            user1.nuevoSeguidor();
 
         user1.mostrarInfo();
+       }
 
-        Usuario user2 = new Usuario("Estudiante");
+       Usuario user2 = new Usuario("Estudiante");
 
          user2.nuevoSeguidor();
          user2.nuevoSeguidor();
          user2.mostrarInfo();
+
+        
     }
 
 }
