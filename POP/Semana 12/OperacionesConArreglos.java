aff8aca417bd9b2f42d27ae04c8d123956462da7
Yerimimd07 <yerimi.miranda@campusucc.edu.co>
2026-04-29 00:52:37 +0000

    Arreglos OK

--- POP/Semana 12/OperacionesConArreglos.java
+++ POP/Semana 12/OperacionesConArreglos.java
@@ -1,30 +1,30 @@
 public class OperacionesConArreglos {
-    
-    //Llenar el arreglo de enteros
-    public int[] llenarArreglo(int[] a, int min, int max){
+
+    // Llenar el arreglo de enteros
+    public int[] llenarArreglo(int[] a, int min, int max) {
         for (int i = 0; i < a.length; i++) {
             a[i] = (int) (Math.random() * (max - min + 1) + min);
         }
         return a;
     }
 
-    //Mostrar el arreglo de enteros
-    public void mostrarArreglo(int[] a){
+    // Mostrar el arreglo de enteros
+    public void mostrarArreglo(int[] a) {
         for (int i = 0; i < a.length; i++) {
-           System.out.println("a["+i+"]="+a[i]); 
+            System.out.print(" a[" + i + "]=" + a[i]);
         }
     }
 
-    //Método para sumar los numeros pares e impares del arreglo de enteros
-    public String sumarParesImpares(int[] a){
+    // Método para sumar los numeros pares e impares del arreglo de enteros
+    public String sumarParesImpares(int[] a) {
         int pares = 0, impares = 0;
         for (int i = 0; i < a.length; i++) {
-            if(a[i] % 2 == 0){
+            if (a[i] % 2 == 0) {
                 pares += a[i];
-            }else{
+            } else {
                 impares += a[i];
             }
         }
-        return "La suma de los pares es: " + pares + " y la de los impares es: " + impares;
-    } 
+        return "\n La suma de los pares es: " + pares + " y la de los impares es: " + impares;
+    }
 }
